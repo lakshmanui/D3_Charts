f6de8b684e9938a7716152d6d2e230dd64db3036
lakshmanui <lakshmanuidev@gmail.com>
2018-04-03 16:07:59 -0500

    all charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import { AppComponent } from './app.component';
+import { LinechartComponent } from './linechart/linechart.component';
+import { BarchatComponent } from './barchat/barchat.component';
+import { DonutChartComponent } from './donut-chart/donut-chart.component';
+import {AppRoutingModule} from './app.routing.module';
+import { MultiLineChartComponent } from './multi-line-chart/multi-line-chart.component';
+import { MultiBarChartComponent } from './multi-bar-chart/multi-bar-chart.component';
+import { GroupChartComponent } from './group-chart/group-chart.component';
+import { AreaChartComponent } from './area-chart/area-chart.component';
+import { WaterFallComponent } from './water-fall/water-fall.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinechartComponent,
+    BarchatComponent,
+    DonutChartComponent,
+    MultiLineChartComponent,
+    MultiBarChartComponent,
+    GroupChartComponent,
+    AreaChartComponent,
+    WaterFallComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
